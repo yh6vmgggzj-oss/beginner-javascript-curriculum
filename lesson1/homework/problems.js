d348127a03d0fd8d4e36f30855c61d19ac9d86ac
yh6vmgggzj-oss <yh6vmgggzj@privaterelay.appleid.com>
2026-08-17 18:42:17 +0000

    Finished lesson 1

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,25 +1,25 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
+console.log(number, 5);
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
+console.log("I sleep for 7 hours");
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
-
+console.log("My favorite fruit is oranges");
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
+console.log("My city is Seattle");
 
-
+console.log("My country is the United States");
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
-
+console.log(name, Sparkle, Age, 1.5, type, Labrador);
