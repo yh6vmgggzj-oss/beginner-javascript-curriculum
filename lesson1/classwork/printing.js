793bebe87ff948a35f247c2d144b792d3b0aa4ff
yh6vmgggzj-oss <yh6vmgggzj@privaterelay.appleid.com>
2026-08-17 17:38:11 +0000

    GGG

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello world!");
+console.log("My name is Terence!");
+console.log('My favorite food is pizza!');
+
+// Error: quotation marks mismatched
+// console.log("I love dogs!');
