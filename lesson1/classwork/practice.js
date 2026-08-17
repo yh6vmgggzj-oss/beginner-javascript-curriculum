671cd05b24add779b7dd1c31ec6806e2c8d0f3fc
yh6vmgggzj-oss <yh6vmgggzj@privaterelay.appleid.com>
2026-08-17 18:24:54 +0000

    GGG

--- lesson1/classwork/practice.js
+++ lesson1/classwork/practice.js
@@ -1,25 +1,26 @@
 // Problem 1
 // Create a variable for your favorite number and print it.
 
-
+console.log("My favorite number, 5");
 
 // Problem 2
 // Create a variable for your height in meters and print it.
-
-
+let x = 1.29;
+console.log(height in Meters);
+x = (height in meters);
 
 // Problem 3
 // Create a variable for your favorite animal and print it in a full sentence.
-
-
+console.log("My favorite animal is a shark.");
 
 // Problem 4
 // Create two variables: one for your first name and one for your last name.
 // Print them on 2 separate lines.
+console.log("My first name is Terence");
 
-
+console.log("My last name is Yuan");
 
 // Problem 5
 // Create 3 variables: your first name, your age, and your favorite color.
 // Print them on the same line in a full sentence.
-
+console.log( name, Terence, age, 9, color, Red);
---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,13 @@
+let x = 5;  // number: whole-number or decimal value
+console.log(x);
+
+let number = 3.97;
+console.log(number);
+
+// Changing the value stored inside the variable
+x = 9;  // You only use let keyword when firts defining your variable
+console.log(x);
+
+let favorite_food = "pizza";    // string: text enclosed in quotes
+console.log(favorite_food);
+console.log("My favorite food is", favorite_food);
